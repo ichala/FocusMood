21468ad2d2dec31caff358f35bac19c9d276df14
Ali Jendoubi <alijandhd@gmail.com>
2023-01-31 17:34:27 +0100

    Install React Icons

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div data-theme="night" className="min-h-screen">
-      Hello
-    </div>
-  );
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div data-theme="night" className="min-h-screen bg-gradient-to-b from-base-100 to-base-300">
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="relative glass w-96 h-96   rounded-full hover:border-none flex justify-center items-center text-center p-5 shadow-xl">
+          <h2 className="text-8xl ">17:00</h2>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
