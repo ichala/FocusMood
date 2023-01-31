f31b2c3698df49f1ef78c97b4698e7cc3396d947
Ali Jendoubi <alijandhd@gmail.com>
2023-01-31 22:17:31 +0100

    Add Watch History

--- src/Context/Preferences.jsx
+++ src/Context/Preferences.jsx
@@ -1,17 +1,23 @@
 import React, { createContext, useState } from 'react';
 
 export const PreferencesContext = createContext();
+const ThemeList = ['cupcake', 'synthwave', 'halloween', 'forest', 'luxury', 'dracula', 'night', 'winter'];
 
 const defaultPreferences = {
   timer: 15,
   theme: 'night',
+  history: [],
 };
 
 export const Preferences = ({ children }) => {
   const [Config, setConfig] = useState(JSON.parse(localStorage.getItem('focus-mood')) || defaultPreferences);
+  const [Video, setVideo] = useState(null);
   localStorage.setItem('focus-mood', JSON.stringify(Config));
   return (
-    <PreferencesContext.Provider value={{ Config, setConfig }}>
+    <PreferencesContext.Provider value={{
+      setVideo, Video, Config, setConfig, ThemeList,
+    }}
+    >
       {children}
     </PreferencesContext.Provider>
   );
