b45f02a32854a8c5dc6a3425283629723ccafb57
Ali Jendoubi <alijandhd@gmail.com>
2023-01-31 17:36:25 +0100

    Update eslint

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,13 @@ function App() {
         <div className="relative glass w-96 h-96   rounded-full hover:border-none flex justify-center items-center text-center p-5 shadow-xl">
           <h2 className="text-8xl ">17:00</h2>
         </div>
-
+        <div className="dropdown dropdown-hover">
+          <label tabIndex={0} className="btn m-1">Hover</label>
+          <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+            <li><a>Item 1</a></li>
+            <li><a>Item 2</a></li>
+          </ul>
+        </div>
       </div>
     </div>
   );
