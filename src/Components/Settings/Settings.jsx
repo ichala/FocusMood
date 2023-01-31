d4cb57c9e6b218618e2d33822ce792abdd54fd58
Ali Jendoubi <alijandhd@gmail.com>
2023-01-31 22:17:07 +0100

    Update Settings page

--- src/Components/Settings/Settings.jsx
+++ src/Components/Settings/Settings.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react';
 import { PreferencesContext } from '../../Context/Preferences';
 
 const Settings = () => {
-  const { Config, setConfig } = useContext(PreferencesContext);
+  const { Config, setConfig, ThemeList } = useContext(PreferencesContext);
   const [Timer, setTimer] = useState(Config.timer);
   return (
     <div>
@@ -33,6 +33,36 @@ const Settings = () => {
           Save
 
         </button>
+
+      </div>
+      <div className="flex flex-col gap-2 m-5">
+        {
+            ThemeList.map((theme) => (
+              <button
+                key={theme}
+                type="button"
+                onClick={
+                  () => {
+                    setConfig({ ...Config, theme });
+                  }
+                }
+              >
+                <div data-theme={theme} className="bg-base-100  text-base-content w-full cursor-pointer font-sans">
+                  <div className="grid grid-cols-5 grid-rows-3">
+                    <div className="col-span-5 row-span-3 row-start-1 flex gap-1 py-3 px-4">
+                      <div className="flex-grow text-sm font-bold">{theme}</div>
+                      <div className="flex flex-shrink-0 flex-wrap gap-1">
+                        <div className="bg-primary w-2 rounded" />
+                        <div className="bg-secondary w-2 rounded" />
+                        <div className="bg-accent w-2 rounded" />
+                        <div className="bg-neutral w-2 rounded" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </button>
+            ))
+          }
       </div>
     </div>
   );
