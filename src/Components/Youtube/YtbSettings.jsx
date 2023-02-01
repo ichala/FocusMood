4d10512b32c13a7dd748783f9baa0876bbbe31a7
Ali Jendoubi <alijandhd@gmail.com>
2023-02-01 19:33:34 +0100

    Update Settings

--- src/Components/Youtube/YtbSettings.jsx
+++ src/Components/Youtube/YtbSettings.jsx
@@ -1,8 +1,67 @@
 import React, { useContext, useState } from 'react';
+import { AiFillYoutube, AiFillFacebook } from 'react-icons/ai';
+import {
+  BsTwitch, BsRainbow, BsFillPlayFill, BsFillPauseFill,
+  BsFillVolumeMuteFill, BsFillVolumeUpFill, BsVolumeDownFill,
+} from 'react-icons/bs';
+import { ImLoop2, ImSoundcloud2 } from 'react-icons/im';
+
+import { FaVimeo } from 'react-icons/fa';
+import { SiMixcloud, SiWistia, SiDailymotion } from 'react-icons/si';
 import { PreferencesContext } from '../../Context/Preferences';
 
+const supportedWebsites = [
+  {
+    name: 'Youtube',
+    url: 'http://youtube.com',
+    icon: <AiFillYoutube className="text-red-500" size={20} />,
+  },
+  {
+    name: 'Twitch',
+    url: 'http://twitch.com',
+    icon: <BsTwitch className="text-purple-600" size={20} />,
+  },
+  {
+    name: 'Facebook',
+    url: 'http://facebook.com',
+    icon: <AiFillFacebook className="text-blue-500" size={20} />,
+  },
+  {
+    name: 'SoundCloud',
+    url: 'http://soundcloud.com',
+    icon: <ImSoundcloud2 className="text-orange-500" size={20} />,
+  },
+  {
+    name: 'Vimeo',
+    url: 'http://vimeo.com',
+    icon: <FaVimeo className="text-black-500" size={20} />,
+  },
+  {
+    name: 'Mixcloud',
+    url: 'http://mixcloud.com',
+    icon: <SiMixcloud className="text-white" size={20} />,
+  },
+  {
+    name: 'Wistia',
+    url: 'http://wistia.com',
+    icon: <SiWistia className="text-blue-500" size={20} />,
+  },
+  {
+    name: 'DailyMotion',
+    url: 'http://dailymotion.com',
+    icon: <SiDailymotion className="text-gray-900" size={20} />,
+  },
+  {
+    name: 'Kaltura',
+    url: 'http://kaltura.com',
+    icon: <BsRainbow className="text-yellow-500" size={20} />,
+  },
+];
+
 const YtbSettings = () => {
-  const { setVideo, Config, setConfig } = useContext(PreferencesContext);
+  const {
+    setVideo, Config, setConfig, VideoControls, setVideoControls,
+  } = useContext(PreferencesContext);
   const [Link, setLink] = useState('');
   return (
     <div>
@@ -10,32 +69,102 @@ const YtbSettings = () => {
         <label className="label">
           <span className="label-text">
             Paste a
-            <b> Youtube </b>
-            link
+            <b> supported </b>
+            video link
           </span>
         </label>
+        <div className="flex gap-2 m-1 items-center justify-start">
+          {supportedWebsites.map((website) => (
+            <a key={website.url} href={website.url} target="_blank" rel="noopener noreferrer">
+              {website.icon}
+            </a>
+          ))}
+
+        </div>
         <input
           type="text"
           onChange={
             (e) => setLink(e.target.value)
           }
           placeholder="www.youtube.com/watch?v=..."
-          className="input input-bordered w-full max-w-xs"
+          className="input input-bordered w-full max-w-xs mt-2"
         />
       </div>
-      <div className="flex justify-center items-center mt-3 w-full">
-        <button
-          onClick={
+      <div className="flex justify-center gap-2 items-center mt-3 w-full">
+        {VideoControls.playing ? (
+          <button
+            onClick={
+              () => setVideoControls({ ...VideoControls, playing: false })
+            }
+            type="button"
+            className="btn btn-ghost btn-xs"
+          >
+            <BsFillPauseFill />
+            Pause
+          </button>
+        ) : (
+          <button
+            onClick={
             () => {
-              setVideo(Link);
-              setConfig({ ...Config, history: [...Config.history, Link] });
+              if (Link !== '') {
+                setVideo(Link);
+                if (!Config.history.includes(Link)) {
+                  setConfig({ ...Config, history: [...Config.history, Link] });
+                }
+                setVideoControls({ ...VideoControls, playing: true });
+              }
             }
           }
-          type="button"
-          className="btn btn-primary btn-sm"
-        >
-          Play
-        </button>
+            type="button"
+            className="btn btn-primary btn-xs"
+          >
+            <BsFillPlayFill />
+            Play
+          </button>
+        )}
+        {
+          !VideoControls.loop ? (
+            <button
+              onClick={
+                () => setVideoControls({ ...VideoControls, loop: true })
+              }
+              type="button"
+              className="btn btn-ghost btn-xs"
+            >
+              <ImLoop2 />
+              Loop
+            </button>
+          ) : (
+            <button
+              onClick={
+                () => setVideoControls({ ...VideoControls, loop: false })
+              }
+              type="button"
+              className="btn btn-primary btn-xs"
+            >
+              <ImLoop2 />
+              Loop
+            </button>
+          )
+        }
+
+        {
+        Number(VideoControls.volume) === 0 ? (<BsFillVolumeMuteFill size={30} className="text-error" />) : (Number(VideoControls.volume) > 0.5 ? <BsFillVolumeUpFill size={30} /> : <BsVolumeDownFill size={30} />)
+        }
+        <input
+          type="range"
+          onChange={
+        (e) => {
+          setVideoControls({ ...VideoControls, volume: e.target.value });
+        }
+      }
+          defaultValue={VideoControls.volume}
+          min={0}
+          max={1}
+          step={0.05}
+          className="range range-xs"
+        />
+
       </div>
 
     </div>
