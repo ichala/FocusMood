002303f8166f55c57ec259a756e6f99db7286b0d
Ali Jendoubi <alijandhd@gmail.com>
2023-02-01 19:33:22 +0100

    Update Player

--- src/Components/Youtube/Player.jsx
+++ src/Components/Youtube/Player.jsx
@@ -3,11 +3,11 @@ import ReactPlayer from 'react-player';
 import { PreferencesContext } from '../../Context/Preferences';
 
 const Player = () => {
-  const { Video } = useContext(PreferencesContext);
+  const { Video, VideoControls } = useContext(PreferencesContext);
 
   return (
     <>
-      <ReactPlayer playing loop className="absolute  left-0 min-w-full min-h-screen" url={Video} />
+      <ReactPlayer volume={VideoControls.volume} playing={VideoControls.playing} loop={VideoControls.loop} className="absolute  left-0 min-w-full min-h-screen" url={Video} />
       <div className="absolute bg-base-300/70 left-0 min-w-full min-h-screen" />
     </>
   );
