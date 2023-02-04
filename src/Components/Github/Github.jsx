e733aed3a0257f32517f66e24f058d6469e54f91
Ali Jendoubi <alijandhd@gmail.com>
2023-02-04 21:41:50 +0100

    Add Water Mark

--- src/Components/Github/Github.jsx
+++ src/Components/Github/Github.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 import { FaGithubSquare } from 'react-icons/fa';
 
 const Github = () => (
-  <div className="absolute glass bottom-4 right-4  bg-primary text-primary-content rounded-md p-1 font-semibold">
-    <a href="https://github.com/ichala/FocusMood" target="_blank" rel="noreferrer">
+  <>
+    <div className="absolute  bottom-4 right-4   text-primary-content  p-1 font-semibold">
+      <a href="https://github.com/ichala/FocusMood" target="_blank" rel="noreferrer">
+        <div className="flex gap-1 items-center">
+          <FaGithubSquare size={30} />
+          Source Code
+        </div>
+      </a>
+    </div>
+    <div className="absolute  bottom-4 left-4   p-1 font-semibold">
       <div className="flex gap-1 items-center">
-        <FaGithubSquare size={30} />
-        Source Code
+        FocusMood.
       </div>
-    </a>
-  </div>
+    </div>
+  </>
 );
 
 export default Github;
